f1d5c4b4fc7f057f61075b1e8161bcdf7f3f730d
Emmanuel <es.shekinah@gmail.com>
2020-08-17 00:29:32 +0200

    image slider app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SimpleImageSlider from "react-simple-image-slider";
 
 function App() {
+
+  //declare images in arrays
+  const images = [
+    { url: "https://www.miga.org/sites/default/files/2018-06/soybean-plants-with-cloudy-sky.jpg" },
+    { url: "https://www.vocfm.co.za/wp-content/uploads/2019/03/SA-LAND-EXPROPRIATION.jpg" },
+    { url: "https://cdn.coingape.com/wp-content/uploads/2020/02/25173413/Screen-Shot-2020-02-25-at-5.32.11-pm-min-678x381.png" },
+    { url: "https://fitsmallbusiness.com/wp-content/uploads/2017/09/How-to-buy-land-ftrd.jpg" },
+];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        {/* Calling image slider package component, and passing array of images */}
+        <SimpleImageSlider
+                    width={896}
+                    height={504}
+                    images={images}
+                />
+      </center>
+     
     </div>
   );
 }
 
+//export the function
 export default App;
